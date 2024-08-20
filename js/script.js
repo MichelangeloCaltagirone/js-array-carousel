5c83f189116a1940aee8a28ca0109b17e87cacd9
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-08-20 17:12:23 +0200

    Add the second bonus consisting implementing the thumbnails and his behaviour when events happen.

--- js/script.js
+++ js/script.js
@@ -1,60 +1,29 @@
-/*
-Consegna:
-Dato un array contenente una lista di cinque immagini, creare un carosello ispirandovi alle foto in allegato.
-Potete anche usare immagini diverse e variare leggermente lo stile, l'importante è la logica!
-
-MILESTONE 1
-Per prima cosa, creiamo il markup statico: costruiamo il container e inseriamo l'immagine grande in modo da poter stilare lo slider;
-avremo così la struttura base e gli stili pronti per poterci poi concentrare solamente sull'aspetto logico.
-
-MILESTONE 2
-Adesso rimuoviamo tutto il markup statico e inseriamo le immagini dinamicamente servendoci dell'array fornito e un semplice ciclo for.
-Possiamo concatenare una stringa con un template literal oppure utilizzare gli altri metodi di manipolazione del DOM che abbiamo visto insieme.
-Al termine di questa fase ci ritroveremo con lo stesso slider, ma costruito dinamicamente attraverso JavaScript.
-
-MILESTONE 3
-Al click dell'utente sulle frecce, l'immagine attiva cambia e diventa visibile nello slider, prendendo il posto della precedente.
-
-BONUS 1:
-Aggiungere il ciclo infinito del carosello. Ovvero se l' immagine attiva è la prima e l'utente clicca la freccia per andare indietro,
-la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se l'utente clicca la freccia verso avanti,
-deve attivarsi la prima immgine.
-
-BONUS 2:
-Creiamo delle miniature di tutte le immagni, in cui dovrà apparire in evidenza l’immagine equivalente a quella attiva,
-scegliete liberamente se scurire le altre immagini oppure se evidenziarla semplicemente con un bordo. Tra queste miniature,
-quella corrispondente all'immagine attiva deve evidenziarsi, scegliete voi l'effetto estetico, potete colorarla diversamente rispetto
-alle altre o aggiungere un semplice bordo.
-
-Prima di partire a scrivere codice:
-Non lasciamoci spaventare dalla complessità apparente dell'esercizio, ma analizziamo prima, come abbiamo fatto sempre,
-cosa ci potrebbe aspettare. Abbiamo completato ormai da qualche settimana la sessione HTML e CSS, se non ci ricordiamo qualcosa
-andiamo pure a riguardare alcuni argomenti. Non dedichiamo però al ripasso più di una mezz'ora, così da non perdere di vista il focus
-dell'esercizio.
-Scriviamo sempre prima per punti il nostro algoritmo in italiano per capire cosa vogliamo fare
-*/
-
-
 // Preprarzione dati noti
 sources = ['img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 'img/05.webp']
 
 // Recupero elementi dal DOM
 const btnPrev = document.getElementById('prev');
 const btnNext = document.getElementById('next');
-const carouselGallery = document.querySelector('.gallery');
+const gallery = document.getElementById('gallery');
+const thumbsGallery = document.getElementById('thumbnails');
 
 let imgs = '' // mi preparo la stringa da montare nel DOM
 
 for (let i = 0; i < sources.length; i++) {
-    imgs += `<img alt="landscape-${i + 1}" src="${sources[i]}">`  // creo la stringa ciclando gli indici dell'array
+    imgs += `<img alt="landscape-${i + 1}" src="${sources[i]}">`  // creo la stringa dinamicamente con gli indici dell'array
 }
-gallery.innerHTML = imgs;    // modifico il DOM
 
-const images = document.querySelectorAll('#carousel img');
+gallery.innerHTML = imgs;           // modifico il DOM
+thumbsGallery.innerHTML = imgs;     // modifico il DOM
+
+const images = document.querySelectorAll('#carousel img');   // recupero le imgs dinamicamente inserite
+const thumbs = document.querySelectorAll('#thumbnails img'); // recupero le imgs dinamicamente inserite
+
 
 // Mostro la prima(nell'array) immagine in pagina
 let currentActiveIndex = 0;
 images[currentActiveIndex].classList.add('active');
+thumbs[currentActiveIndex].classList.add('active');
 
 
 // Eventi dinamici 
@@ -62,23 +31,26 @@ images[currentActiveIndex].classList.add('active');
 btnPrev.addEventListener('click', function() {
     // tolgo classe attiva
     images[currentActiveIndex].classList.remove('active');
-
+    thumbs[currentActiveIndex].classList.remove('active');
     currentActiveIndex--;
     // dopo il decremento, controllo di non uscire dall'array, in tal caso lo riporto alla fine
     if (currentActiveIndex < 0) currentActiveIndex = images.length - 1;
-
+     // rimetto classe attiva, all'indice giusto
     images[currentActiveIndex].classList.add('active');
+    thumbs[currentActiveIndex].classList.add('active');
 })
+
 // Next btn
 btnNext.addEventListener('click', function() {
     // tolgo classe attiva
     images[currentActiveIndex].classList.remove('active');
-
+    thumbs[currentActiveIndex].classList.remove('active');
     currentActiveIndex++;
     // dopo l'incremento, controllo di non uscire dall'array, in tal caso lo riporto all'inizio
     if (currentActiveIndex === images.length) currentActiveIndex = 0;
-
+    // rimetto classe attiva, all'indice giusto
     images[currentActiveIndex].classList.add('active');
+    thumbs[currentActiveIndex].classList.add('active');
 })
 
 
